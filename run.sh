18e4c40e55787e3ea892341025927e85fb6afc07
Mohamad Gebai <mohamad.gebai@gmail.com>
2016-11-01 11:49:04 -0400

    Make it run longer to reach the steady state
    Signed-off-by: Mohamad Gebai <mohamad.gebai@gmail.com>

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ lttng create --snapshot -o .
 lttng enable-channel chan0 --subbuf-size 8k --num-subbuf 2 -u
 lttng enable-event -u -c chan0 ustbench:bench_tp_4b
 lttng start 
-./bin/ustbench -n 15000 -p 1 -t lttng-ust
+./bin/ustbench -n 55000 -p 1 -t lttng-ust
 lttng stop
 
